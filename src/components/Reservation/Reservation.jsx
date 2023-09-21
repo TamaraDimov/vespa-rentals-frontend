654230dd8f569e276ff4c863ab66ad44f37dcd63
kifle23 <kiflekifle23@gmail.com>
2023-09-21 18:40:15 +0300

    Generate Reservation component

---
+++ src/components/Reservation/Reservation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Reservation = () => (
+  <div>
+    Reservation Component
+  </div>
+);
+
+Reservation.propTypes = {};
+
+Reservation.defaultProps = {};
+
+export default Reservation;
