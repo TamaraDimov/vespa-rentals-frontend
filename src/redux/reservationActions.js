418fa90046ef79521b04ae82139605a7e5ae1561
kifle23 <kiflekifle23@gmail.com>
2023-09-22 16:08:21 +0300

    Render reservation components

--- src/redux/reservationActions.js
+++ src/redux/reservationActions.js
@@ -1,42 +1,42 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import API_URL from '../App/API_URL';
+import API_URL from '../app/API_URL';
 
 export const fetchReservations = createAsyncThunk(
-    'reservations/fetchReservations',
-    async () => {
-        try {
-            const response = await fetch(`${API_URL}/reservations`);
-            if (!response.ok) {
-                throw new Error('Failed to fetch reservations');
-            }
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            throw new Error(error.message);
-        }
+  'reservations/fetchReservations',
+  async () => {
+    try {
+      const response = await fetch(`${API_URL}/reservations`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch reservations');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error(error.message);
     }
+  },
 );
 
 export const addReservation = createAsyncThunk(
-    'reservations/addReservation',
-    async (reservation) => {
-        try {
-            const response = await fetch(`${API_URL}/reservations`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(reservation),
-            });
-            if (!response.ok) {
-                throw new Error('Failed to add reservation');
-            }
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            throw new Error(error.message);
-        }
+  'reservations/addReservation',
+  async (reservation) => {
+    try {
+      const response = await fetch(`${API_URL}/reservations`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(reservation),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to add reservation');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error(error.message);
     }
+  },
 );
 
 export default { fetchReservations, addReservation };
