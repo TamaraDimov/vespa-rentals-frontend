906956a40aff20b779a2ac582054e3e88c270ca5
kifle23 <kiflekifle23@gmail.com>
2023-09-22 07:54:56 +0300

    Add show reservations

--- src/components/Reservation/Reservation.jsx
+++ src/components/Reservation/Reservation.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchReservations } from "../../redux/reservationActions";
 
+const Reservations = () => {
+  const reservations = useSelector((state) => state.reservations);
+  const dispatch = useDispatch();
 
-const Reservation = () => (
-  <div>
-    Reservation Component
-  </div>
-);
+  useEffect(() => {
+    dispatch(fetchReservations());
+  }, [dispatch]);
 
-Reservation.propTypes = {};
+  return (
+    <div>
+      <h1>Reservations</h1>
+      {reservations.loading && <p>Loading...</p>}
+      {reservations.error && (
+        <>
+          <p>Error:</p>
+          <p>{reservations.error}</p>
+        </>
+      )}
+      {!reservations.loading && !reservations.error && (
+        <table>
+          <thead>
+            <tr>
+              <th>Reservation ID</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>City</th>
+              <th>User ID</th>
+              <th>Motorcycle ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reservations.data.map((reservation) => (
+              <tr key={reservation.id}>
+                <td>{reservation.id}</td>
+                <td>{reservation.start_date}</td>
+                <td>{reservation.end_date}</td>
+                <td>{reservation.city}</td>
+                <td>{reservation.user_id}</td>
+                <td>{reservation.motorcycle_id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 
-Reservation.defaultProps = {};
-
-export default Reservation;
+export default Reservations;
