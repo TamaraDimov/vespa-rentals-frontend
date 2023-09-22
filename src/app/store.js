b1eefd1479a9dbaae9bc45c76419a592d188556c
kifle23 <kiflekifle23@gmail.com>
2023-09-22 07:53:49 +0300

    Update store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import motorcycleReducer from '../components/motorcycle/motorcycleSlice';
+import reservationReducer from '../redux/reservationSlice';
 
 export default configureStore({
   reducer: {
     motorcycle: motorcycleReducer,
+    reservation: reservationReducer,
   },
 });
