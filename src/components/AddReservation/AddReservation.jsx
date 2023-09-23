f2c5eb26e42bf744b3ab4ef83c08b363609da233
kifle23 <kiflekifle23@gmail.com>
2023-09-23 10:12:46 +0300

    Update add reservation to fetch motorcycle

--- src/components/AddReservation/AddReservation.jsx
+++ src/components/AddReservation/AddReservation.jsx
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { addReservation } from '../../redux/reservationActions';
+import { getUserFromLocalStorage } from '../../helpers/LocalStorage';
+import { fetchMotorcycle } from '../../redux/reducers/motorcycleSlice';
+
+const data = getUserFromLocalStorage();
 
 function AddReservation() {
   const dispatch = useDispatch();
@@ -8,9 +12,18 @@ function AddReservation() {
     start_date: '',
     end_date: '',
     city: '',
-    user_id: '',
+    user_id: data.user.data.id,
     motorcycle_id: '',
   });
+  useSelector((state) => state.motorcycle);
+
+  useEffect(() => {
+    dispatch(fetchMotorcycle());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchMotorcycle());
+  }, [dispatch]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -27,17 +40,24 @@ function AddReservation() {
       start_date: '',
       end_date: '',
       city: '',
-      user_id: '',
       motorcycle_id: '',
     });
   };
+
+  const cities = [
+    'New York',
+    'Los Angeles',
+    'Chicago',
+    'Houston',
+    'Philadelphia',
+  ];
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="start_date">
         Start Date:
         <input
-          type="text"
+          type="date"
           name="start_date"
           id="start_date"
           value={reservation.start_date}
@@ -47,7 +67,7 @@ function AddReservation() {
       <label htmlFor="end_date">
         End Date:
         <input
-          type="text"
+          type="date"
           name="end_date"
           id="end_date"
           value={reservation.end_date}
@@ -56,33 +76,30 @@ function AddReservation() {
       </label>
       <label htmlFor="city">
         City:
-        <input
-          type="text"
-          name="city"
-          id="city"
-          value={reservation.city}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="user_id">
-        User ID:
-        <input
-          type="text"
-          name="user_id"
-          id="user_id"
-          value={reservation.user_id}
-          onChange={handleChange}
-        />
+        <select name="city" value={reservation.city} onChange={handleChange}>
+          <option value="">Select a city</option>
+          {cities.map((city) => (
+            <option key={city} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
       </label>
       <label htmlFor="motorcycle_id">
         Motorcycle ID:
-        <input
-          type="text"
-          name="motorcycle_id"
+        <select
+          name="Motorcycle"
           id="motorcycle_id"
           value={reservation.motorcycle_id}
           onChange={handleChange}
-        />
+        >
+          {/* <option value="">Select a motorcycle</option>
+          {motorcycle.motorcycle.map((motor) => (
+            <option key={motor.id} value={motor.id}>
+              {motor.name}
+            </option>
+          ))} */}
+        </select>
       </label>
       <button type="submit">Add Reservation</button>
     </form>
