81cb1b3a19ec7a7e8b7c58ea2b206ec44f4e1b8f
kifle23 <kiflekifle23@gmail.com>
2023-09-23 18:29:33 +0300

    Add delete motorcycle component

---
+++ src/components/motorcycle/DeleteMotorcycle.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMotorcycle, deleteMotorcycle } from '../../redux/reducers/motorcycleSlice';
+
+const DeleteMotorcycle = () => {
+  const motorcycles = useSelector((state) => state.motorcycle);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMotorcycle());
+  }, [dispatch]);
+
+  const handleDelete = (id) => {
+    dispatch(deleteMotorcycle(id));
+  };
+
+  const available = motorcycles.length;
+
+  const message = `We have ${available} motorcycle${available !== 1 ? 's' : ''} available.
+  ${available ? 'Select a motorcycle down below to delete!' : 'No motorcycles are available!'}`;
+
+  return (
+    <section>
+      <div>
+        <h1>Available motorcycles</h1>
+        <p>{message}</p>
+        <div>
+          {motorcycles.map((motorcycle) => (
+            <div key={motorcycle.id}>
+              <img src={motorcycle.photo} alt={motorcycle.model} />
+              <div>
+                <h2>
+                  {motorcycle.name}
+                </h2>
+                <p>
+                  {motorcycle.description}
+                </p>
+                <button type="button" onClick={() => handleDelete(motorcycle.id)}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DeleteMotorcycle;
