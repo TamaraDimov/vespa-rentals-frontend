68762d9e01328c7077802a25295f1b337d84198e
TamaraDimov <tamemojsoska@gmail.com>
2023-09-24 01:11:08 +0200

    add book a ride link

--- src/components/Sidebar/links.js
+++ src/components/Sidebar/links.js
@@ -1,6 +1,7 @@
 const links = [
   { id: 1, text: 'Models', path: '/motorcycles' },
-  { id: 2, text: 'Reservations', path: '/Reservation' },
+  { id: 2, text: 'Book a ride', path: '/AddReservation' },
+  { id: 2, text: 'My Reservations', path: '/Reservation' },
   { id: 3, text: 'ADD Motorcycle', path: '/' },
   { id: 4, text: 'DELETE Motorcycle', path: '/motorcycles/delete-motorcycles' },
 ];
