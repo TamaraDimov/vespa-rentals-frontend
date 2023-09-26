e5c19faafe2038f5204f85f3fd69d0acb1a9ffbf
danielsafari143 <danielsafari143@gmail.com>
2023-09-26 10:03:53 +0200

    MODIFY DELETEMOTORCYCLE COMP

--- src/components/motorcycle/DeleteMotorcycle.jsx
+++ src/components/motorcycle/DeleteMotorcycle.jsx
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   fetchMotorcycle,
   deleteMotorcycle,
 } from '../../redux/reducers/motorcycleSlice';
-import PopDelete from './subcomponents/pop/Delete';
 
 const DeleteMotorcycle = () => {
   const motorcycles = useSelector((state) => state.motorcycle);
   const { user } = useSelector((state) => state.user);
-  const [confirm, setConfirm] = useState(false);
-  const [id, setId] = useState('');
   const { token } = user.user;
 
   const dispatch = useDispatch();
@@ -21,10 +18,6 @@ const DeleteMotorcycle = () => {
 
   const handleDelete = (id) => {
     dispatch(deleteMotorcycle(id));
-  };
-
-  const handleConfirmation = () => {
-    setConfirm(!confirm);
   };
 
   const available = motorcycles.motorcycle.length;
@@ -43,7 +36,6 @@ const DeleteMotorcycle = () => {
       <div>
         <h1>Available motorcycles</h1>
         <p>{message}</p>
-        {confirm && <PopDelete id={id} /> }
         <div>
           {Array.isArray(motorcycles.motorcycle)
             && motorcycles.motorcycle.map((motorcycle) => (
@@ -54,10 +46,7 @@ const DeleteMotorcycle = () => {
                   <p>{motorcycle.description}</p>
                   <button
                     type="button"
-                    onClick={() => {
-                      setId(motorcycle.id);
-                      handleConfirmation();
-                    }}
+                    onClick={() => handleDelete(motorcycle.id)}
                   >
                     Delete
                   </button>
--- src/components/motorcycle/subcomponents/pop/Delete.jsx
+++ src/components/motorcycle/subcomponents/pop/Delete.jsx
@@ -1,17 +1,40 @@
-
+import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { deleteMotorcycle } from '../../../../redux/reducers/motorcycleSlice';
 
+export default function PopDelete({ id }) {
+  const [delet, setDelet] = useState(false);
+  const dispatch = useDispatch();
 
+  const handleClick = () => (delet && dispatch(deleteMotorcycle(id)));
+  console.log(id);
 
-export default function PopDelete({id}) {
-  
   return (
     <div>
       <h1>Do you want to delete ?</h1>
-      <button type="button">Cancel</button>
-      <button type="button">Delete</button>
+      <button
+        type="button"
+        onClick={() => {
+          setDelet(false);
+          handleClick();
+        }}
+      >
+        Cancel
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setDelet(true);
+          handleClick();
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
 }
+
+PopDelete.propTypes = {
+  id: PropTypes.number.isRequired,
+};
